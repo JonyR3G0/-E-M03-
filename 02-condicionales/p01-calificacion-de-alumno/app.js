8ecb85073020bbf672028ac1b76db62bb70a06e3
J0n4s4n <jonh.reyes.go@gmail.com>
2025-02-06 22:13:16 -0600

    update app.js

--- 02-condicionales/p01-calificacion-de-alumno/app.js
+++ 02-condicionales/p01-calificacion-de-alumno/app.js
@@ -3,19 +3,25 @@
 // Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
 // Si la nota es menor de 60, el estudiante no aprueba.
 
-let calificacion = prompt("Introduce tu calificacion, usando un numero del 1-100");
+let calificacion = prompt("Introduce tu calificación, usando un número del 1-100");
 
+//Primero detecta si el numero esta dentro del rango
 if (calificacion >= 0 && calificacion <= 100) {
+
   if (calificacion >= 90) {
-    alert("Excelente!");
+    alert("🥇 Excelente ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧ 🥇");
+
   } else if (calificacion >= 75) {
-    alert("Bien");
+    alert("✅ Bien ✅");
+
   } else if (calificacion >= 60) {
-    alert("Suficiente");
+    alert("🤏🏻 Suficiente 🤏🏻");
+  
   } else if (calificacion < 60) {
-    alert("No aprueba");
+    alert("❌ No aprueba (·•᷄‎ࡇ•᷅ ) ❌");
   }
 }
+//Cualquier otro caso que no sea un numero del 1-100
 else{
-    alert('Introduce un numero del 1 - 100')
+    alert('Introduce un número del 1 - 100')
 }
