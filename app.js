74d02da90c27916a0c9e10a9a43b68503f4b95ac
J0n4s4n <jonh.reyes.go@gmail.com>
2025-03-17 00:19:06 -0600

    update app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+document.addEventListener('DOMContentLoaded', mainFunction)
+function mainFunction()
+{
 const heroPowerButton = document.getElementById("HEROPOWER");
-const heroSection = document.querySelector(".hero");
+const heroSection = document.querySelector(".navBar");
+
+const logoString = ['<span class=', '"codeHero"', '>${</span>','project', '<span class=', '"codeHero">','.','</span>','<span class="codeHeroII">','hubIII</span><span class="codeHero">','}','</span>' ];
+
+let strigHTML = '';
+
+stringLogo()
+
+function stringLogo(){
+  for (let i = 0; i < logoString.length; i++) {
+   heroSection.innerHTML += logoString[i];
+  }
+}
+
+console.log(strigHTML)
 
 function easy() {
   console.log("Clicked");
@@ -7,10 +24,11 @@ function easy() {
   //heroSection.classList.replace('hero', 'navBar')
 }
 function HeroKiller() {
-  if (window.scrollY > 90) {
+  if (window.pos > 1) {
     heroSection.classList.replace("hero", "navBar");
   }
 }
 
-heroPowerButton.addEventListener("click", easy);
+// heroPowerButton.addEventListener("click", easy);
 window.addEventListener("scroll", HeroKiller);
+}
