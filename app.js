1ef2a101570ec2aad192ea72b21eac21a16e9b04
J0n4s4n <jonh.reyes.go@gmail.com>
2025-03-18 23:06:09 -0600

    update app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
-document.addEventListener('DOMContentLoaded', mainFunction)
-function mainFunction()
-{
-const heroPowerButton = document.getElementById("HEROPOWER");
-const heroSection = document.querySelector(".navBar");
+const listelements = document.getElementsByTagName("li");
+const inputComment = document.getElementById("inputComentario");
+const btnComment = document.getElementsByTagName("button")[0];
+const btnDelComment = document.getElementsByTagName("button")[1];
+const bxComment = document.getElementById("comentarios");
 
-const logoString = ['<span class=', '"codeHero"', '>${</span>','project', '<span class=', '"codeHero">','.','</span>','<span class="codeHeroII">','hubIII</span><span class="codeHero">','}','</span>' ];
+//creo un array vacio para almacenar los comentarios
+const comentarios = [];
 
-let strigHTML = '';
+let comentar = (event) => {
+  //Creamos la instancia date
+  const date = new Date();
+  //Deconstruimos para tener a la mano los datos
+  const [month, day, year, hour, minutes, seconds] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
+    //To string + pad start es para que cuando retorne un solo digito de muestre un 0 a la izq. para mantener el formato 24 horas.
+    date.getHours().toString().padStart(2, "0"),
+    date.getMinutes().toString().padStart(2, "0"),
+    date.getSeconds().toString().padStart(2, "0"),
+  ];
 
-stringLogo()
+  event.preventDefault(); //solo se usa en formularios para evitar que se ejecute la accion por defecto (reload)
 
-function stringLogo(){
-  for (let i = 0; i < logoString.length; i++) {
-   heroSection.innerHTML += logoString[i];
+  //para prevenir comentar vacio
+  if (inputComment.value === "") {
+    alert("Por favor, no dejes campos vacios (ㅅ´ ˘ `)");
+  } else {
+    console.log(inputComment.value);
+    comentarios.push(
+      `<li>${inputComment.value} <br><i>Comentado el ${day}/${month}/${year} a las ${hour}:${minutes}:${seconds}</i></li>`
+    );
+    bxComment.innerHTML = comentarios.join("");
+    inputComment.value = "";
   }
-}
+};
 
-console.log(strigHTML)
+//Funcion para eliminar el ultimo elemento del array que almacena los comentarios.
+let eliminarCom = (event) => {
+  event.preventDefault();
+  comentarios.pop();
+  bxComment.innerHTML = comentarios.join("");
+};
 
-function easy() {
-  console.log("Clicked");
-  heroPowerButton.classList.replace("HEROPOWER", "invisible");
-  //heroSection.classList.replace('hero', 'navBar')
-}
-function HeroKiller() {
-  if (window.pos > 1) {
-    heroSection.classList.replace("hero", "navBar");
-  }
-}
-
-// heroPowerButton.addEventListener("click", easy);
-window.addEventListener("scroll", HeroKiller);
-}
+//creo listeners para los botones
+btnDelComment.addEventListener("click", eliminarCom);
+btnComment.addEventListener("click", comentar);
